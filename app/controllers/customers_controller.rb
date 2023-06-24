a45b61ad89272d53dc56143d6a8e980cfda61267
Pawanpreet9 <pawanpreetkaur911@gmail.com>
2023-06-24 16:16:24 -0700

    Part-2 new controller setup

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,12 @@
+class CustomersController < ApplicationController
+  def index
+    @customers = Customer.all
+  end
+  def alphabetized
+    @customers = Customer.order(:full_name)
+  end
+  def missing_email
+    @customers = Customer.where(email: [nil, ""]).all
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root to: "home#index"
+ # root to: "home#index"
+ get '/', to: 'customers#index'
+ get '/customers/missing_email', to: 'customers#missing_email'
+ get '/customers/alphabetized', to: 'customers#alphabetized'
 
 end
